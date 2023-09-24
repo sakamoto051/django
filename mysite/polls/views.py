907f1047623174191f158d713dd58fecd935cf61
sakamoto <sakamoto051eng@gmail.com>
2023-09-24 13:51:26 +0000

    feat: new view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', ' . join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse('Youre looking at question%s.' % question_id)
+
+def results(request, question_id):
+    response = 'Your lokking at the results of question %s.' 
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Youre voting on question %s.' % question_id)
